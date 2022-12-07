8c75e29586d89fd3e59e2d095cd44d660f2f1d19
Darksied95 <aliyuefe9501@gmail.com>
2022-12-07 18:32:02 +0100

    added delete endpoints for tasks and users

--- src/index.js
+++ src/index.js
@@ -63,9 +63,20 @@ app.patch('/users/:id', async (req, res) => {
     } catch (e) {
         return res.status(400).send(e)
     }
-
-
 })
+
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
+        if (!user) {
+            return res.send(404).send('User not found')
+        }
+        res.send(user)
+    } catch (err) {
+        return res.send(err)
+    }
+})
+
 
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
@@ -100,6 +111,39 @@ app.get('/tasks/:id', async (req, res) => {
         res.send(task)
     } catch (e) {
         res.status(500).send()
+    }
+})
+
+app.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isUpdatesValid = updates.every(update => allowedUpdates.includes(update))
+
+
+    if (!isUpdatesValid) {
+        return res.status(400).send({ error: 'Invalid update' })
+    }
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        if (!task) {
+            return res.status(404).send('User not found')
+        }
+
+        res.send(task)
+    } catch (e) {
+        return res.status(400).send(e)
+    }
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+        if (!task) {
+            return res.send(404).send('User not found')
+        }
+        res.send(task)
+    } catch (err) {
+        return res.send(err)
     }
 })
 
